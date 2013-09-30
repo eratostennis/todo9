2dc55059c50d32c5bfa9072b105d6ae46f0d000f
eratostennis <eratostennis.school@gmail.com>
2013-09-30 14:57:35 +0900

    merge web client and android client

--- lib/todo9/Web.pm
+++ lib/todo9/Web.pm
@@ -41,6 +41,122 @@ filter 'set_title' => sub {
     }
 };
 
+get '/' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+        'offset' => {
+            default => 0,
+            rule => [
+                ['UINT','ivalid offset value'],
+            ],
+        },
+    ]);
+    $c->halt(403) if $result->has_error;
+    my ($entries,$has_next) = $self->entry_list($result->valid('offset'));
+    $c->render('index.tx', {
+        offset => $result->valid('offset'),
+        entries => $entries,
+        has_next => $has_next,
+    });
+};
+
+get '/edit' => sub {
+    my ( $self, $c )  = @_;
+
+    $c->render('edit.tx', {
+
+    });
+};
+
+post '/edit' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+	'id' => {
+	    rule => [
+		['NOT_NULL', 'empty id'],
+    	    ],
+    	},
+    	'body' => {
+	    rule => [
+		['NOT_NULL', 'empty body'],
+	    ],
+    	}
+    ]);
+    $c->halt(403) if $result->has_error;
+
+    my $id = $result->valid('id');
+    my $body = $result->valid('body');
+
+    $c->render('edit.tx', {
+        # offset => $result->valid('offset'),
+        id => $id,
+        body => $body,
+    });
+};
+
+post '/create' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+        'body' => {
+            rule => [
+                ['NOT_NULL','empty body'],
+            ],
+        },
+        'nickname' => {
+            default => 'anonymous',
+            rule => [
+                ['NOT_NULL','empty nickname'],
+            ],
+        }
+    ]);
+    if ( $result->has_error ) {
+        return $c->render_json({ error => 1, messages => $result->errors });
+    }
+    my $id = $self->add_entry(map {$result->valid($_)} qw/body nickname/);
+    $c->render_json({ error => 0, location => $c->req->uri_for("/")->as_string });
+};
+
+post '/update' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+	'id' => {
+	    rule => [
+		['NOT_NULL', 'empty id'],
+    	    ],
+    	},
+    	'body' => {
+	    rule => [
+		['NOT_NULL', 'empty body'],
+	    ],
+    	}
+    ]);
+    $c->halt(403) if $result->has_error;
+
+    if ( $result->has_error ) {
+        return $c->render_json({ error => 1, messages => $result->errors });
+    }
+    my $id = $self->edit_entry(map {$result->valid($_)} qw/id body/);
+    $c->redirect('/');
+};
+
+post '/delete' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+	'id' => {
+	    rule => [
+		['NOT_NULL', 'empty id'],
+    	    ],
+    	}
+    ]);
+    $c->halt(403) if $result->has_error;
+
+    if ( $result->has_error ) {
+        return $c->render_json({ error => 1, messages => $result->errors });
+    }
+    my $id = $self->delete_entry(map {$result->valid($_)} qw/id/);
+    $c->redirect('/');
+};
+
 get '/api/todos' => sub {
     my ( $self, $c )  = @_;
     my $result = $c->req->validator([
@@ -63,6 +179,8 @@ get '/api/todos' => sub {
 router 'PUT' => '/api/todos/:id' => sub {
     my ( $self, $c )  = @_;
     my $id = $c->args->{id};
+    warn $id;
+    warn $c->req->content;
     my $todo = decode_json($c->req->content)->{todo};
     warn $todo;
 
